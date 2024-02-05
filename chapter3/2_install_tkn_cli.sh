6e16cffa4352f84fa8f5790633007881d069e4f9
arunvel1988 <aryan.arunachalam@gmail.com>
2024-02-05 10:58:32 +0530

    adding code chapter three yamld with purpose and adding more details

--- chapter3/2_install_tkn_cli.sh
+++ chapter3/2_install_tkn_cli.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-
-# Script to install the tkn tool on a Kubernetes cluster
-
-# Specify the desired tkn version
-TKN_VERSION="v0.19.0"
-
-# Download tkn binary
-wget "https://github.com/tektoncd/cli/releases/download/${TKN_VERSION}/tkn_$(uname | tr '[:upper:]' '[:lower:]')_amd64" -O tkn
-
-# Make it executable
-chmod +x tkn
-
-# Move to a directory in your PATH
-sudo mv tkn /usr/local/bin/
-
-# Verify installation
-tkn version
-#!/bin/bash
 #Book Name: Cloud Native CI/CD with Tekton & ArgoCD
 #Authors: Arunvel Arunachalam/ Ajay Arunachalam
 #Purpose: # Script to install the tkn tool on a Kubernetes cluster
